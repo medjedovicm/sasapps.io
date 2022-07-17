c04139a0df41b8b2bb20f8498e0e97418ecd98ee
munja <allnbwe@gmail.com>
2022-07-17 16:40:25 +0100

    chore: automated commit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,8 +38,8 @@ const About: React.FC<Props> = ({ location }: Props) => {
         <p className="custom-text">
           The SAS Apps team have been together (in various forms) since 2018.{' '}
           <br /> We built the SASjs Framework, Data Controller for SAS, and
-          Sasensei.com - not to mention, numerous customer projects. <br /> We
-          usually work through SAS partners, but we can also be engaged
+          Sasensei.com - not to mention, numerous customer projects. <br /> You
+          can engage us through your preferred SAS partner, or contact us
           directly.
         </p>
         <div className="row">
